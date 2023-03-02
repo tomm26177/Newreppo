ea56f4bd7ef749d5482bb5912cb4511bf9439acf
Tomasz Kaminski <tomasz77ka@gmail.com>
2023-03-02 20:39:56 +0100

    commit

--- java_git_program_2.18.2023/src/Main.java
+++ java_git_program_2.18.2023/src/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("hi john");
 
-        System.out.println("hello Git");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
     }
 }
